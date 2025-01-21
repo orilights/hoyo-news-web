04f5c59c331afb032c55e027e517864e7be39096
OriLight <61224409+orilights@users.noreply.github.com>
2025-01-21 23:04:34 +0800

    feat: 新增时间范围筛选、升降序显示选择

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import NewsItem from '@/components/NewsItem.vue'
 import Switch from '@/components/Switch.vue'
-import { APP_ABBR, NEWS_CLASSIFY_RULE, NEWS_LIST, REPO_URL, SHADOW_ITEM, TAG_ALL, TAG_OTHER, TAG_VIDEO } from '@/constants'
+import { APP_ABBR, NEWS_CLASSIFY_RULE, NEWS_LIST, SHADOW_ITEM, TAG_ALL, TAG_OTHER, TAG_VIDEO } from '@/constants'
 import { Settings, SettingType } from '@orilight/vue-settings'
 import { useElementBounding, useElementSize, useThrottle, useUrlSearchParams } from '@vueuse/core'
 import { useToast } from 'vue-toastification'
@@ -22,11 +22,15 @@ const filterTag = ref(TAG_ALL)
 const source = ref(Object.keys(NEWS_LIST)[0])
 const channal = ref('')
 const searchStr = ref('')
+const loading = ref(false)
+
 const showSetting = ref(false)
 const showCover = ref(true)
 const showDateWeek = ref(false)
-const sortNews = ref(true)
-const loading = ref(false)
+const sortByDate = ref(true)
+const sortBy = ref('desc')
+const filterStartDate = ref('')
+const filterEndDate = ref('')
 
 const searchEnabled = computed(() => searchStr.value.trim() !== '')
 
@@ -51,8 +55,14 @@ const filteredNewsData = computed(() => {
   else {
     data = newsData.value.filter(news => news.tag === filterTag.value)
   }
+  if (filterStartDate.value) {
+    data = data.filter(news => new Date(news.startTime).getTime() >= new Date(`${filterStartDate.value} 00:00:00`).getTime())
+  }
+  if (filterEndDate.value) {
+    data = data.filter(news => new Date(news.startTime).getTime() <= new Date(`${filterEndDate.value} 23:59:59`).getTime())
+  }
 
-  if (sortNews.value) {
+  if (sortByDate.value) {
     data = data.sort((a, b) => {
       const bt = new Date(b.startTime).getTime()
       const at = new Date(a.startTime).getTime()
@@ -61,6 +71,8 @@ const filteredNewsData = computed(() => {
       return bt - at
     })
   }
+  if (sortBy.value === 'asc')
+    data.reverse()
 
   data.forEach((v, i) => {
     (v as NewsItemData).top = (itemHeight.value + 8) * i
@@ -90,8 +102,9 @@ onMounted(() => {
       showSetting.value = false
   })
   settings.register('showCover', showCover, SettingType.Bool)
-  settings.register('sortNews', sortNews, SettingType.Bool)
   settings.register('showDateWeek', showDateWeek, SettingType.Bool)
+  settings.register('sortNews', sortByDate, SettingType.Bool)
+  settings.register('sortBy', sortBy, SettingType.Str)
   if (params.filterTag)
     filterTag.value = params.filterTag as string
   if (params.source)
@@ -255,7 +268,7 @@ function scrollTo(target: 'top' | 'bottom') {
             <span class="flex-1">显示封面</span> <Switch v-model="showCover" class="ml-2" />
           </div>
           <div class="my-1 flex items-center">
-            <span class="flex-1">根据发布时间排序</span> <Switch v-model="sortNews" class="ml-2" />
+            <span class="flex-1">根据发布时间排序</span> <Switch v-model="sortByDate" class="ml-2" />
           </div>
           <div class="my-1 flex items-center">
             <span class="flex-1">发布时间显示星期</span> <Switch v-model="showDateWeek" class="ml-2" />
@@ -271,7 +284,7 @@ function scrollTo(target: 'top' | 'bottom') {
           米哈游官网新闻检索
         </h1>
         <div class="flex gap-4">
-          <a :href="REPO_URL" target="_blank">
+          <a href="https://github.com/orilights/hoyo-news-search" target="_blank">
             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 1024 1024" stroke="currentColor" class="size-6">
               <path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9 23.5 23.2 38.1 55.4 38.1 91v112.5c0.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z" />
             </svg>
@@ -336,18 +349,48 @@ function scrollTo(target: 'top' | 'bottom') {
         v-model="searchStr" type="text" placeholder="搜些什么吧"
         class="mb-4 w-full rounded-full border px-4 py-2 outline-blue-500 transition-colors hover:border-blue-500"
       >
-      <ul v-show="!searchEnabled" class="mb-4 flex flex-wrap gap-1">
-        <li
-          v-for="tag in Object.keys(tags).sort((a, b) => tags[b] - tags[a])" :key="tag"
-          class="inline-block cursor-pointer rounded-full border border-gray-400 px-2 py-0.5 text-xs transition-colors hover:border-blue-400 hover:text-blue-500 md:px-3 md:py-1 md:text-sm"
-          :class="{
-            '!border-blue-500 !bg-blue-500 !text-white': filterTag === tag,
-          }"
-          @click="handleClickTag(tag)"
-        >
-          {{ tag }} {{ tags[tag] }}
-        </li>
-      </ul>
+      <details v-show="!searchEnabled" class="mb-4" open>
+        <summary>分类</summary>
+        <ul class="mt-2 flex flex-wrap gap-1">
+          <li
+            v-for="tag in Object.keys(tags).sort((a, b) => tags[b] - tags[a])" :key="tag"
+            class="inline-block cursor-pointer rounded-full border border-gray-400 px-2 py-0.5 text-xs transition-colors hover:border-blue-400 hover:text-blue-500 md:px-3 md:py-1 md:text-sm"
+            :class="{
+              '!border-blue-500 !bg-blue-500 !text-white': filterTag === tag,
+            }"
+            @click="handleClickTag(tag)"
+          >
+            {{ tag }} {{ tags[tag] }}
+          </li>
+        </ul>
+      </details>
+
+      <div class="mb-2 flex flex-wrap items-center gap-x-2 gap-y-1">
+        <span>
+          排序：
+          <select
+            v-model="sortBy"
+            class="rounded-md border border-black/20 bg-transparent px-1"
+          >
+            <option value="desc">
+              降序
+            </option>
+            <option value="asc">
+              升序
+            </option>
+          </select>
+        </span>
+
+        <span>
+          开始日期：
+          <input v-model="filterStartDate" type="date" class="rounded-md border border-black/20 bg-transparent px-1" :max="filterEndDate">
+        </span>
+
+        <span>
+          结束日期：
+          <input v-model="filterEndDate" type="date" class="rounded-md border border-black/20 bg-transparent px-1" :min="filterStartDate">
+        </span>
+      </div>
 
       <div v-show="searchEnabled" class="pb-4">
         <span class="pr-4">
